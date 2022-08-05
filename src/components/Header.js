4c329f1e0a0f6847e0109b3acc8d28b16264bc48
ke-kishimoto <nebinosuk@gmail.com>
2022-08-05 23:36:18 +0900

    メニューアイコン追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
     return (
         <Grid container spacing={2} margin={1}>
             <Grid item xs={1}>
-                メニュー
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                <MenuIcon />
+            </IconButton>
             </Grid>
             <Grid item xs={8} textAlign={'center'}>
                 タイトル
