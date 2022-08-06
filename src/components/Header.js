348af5ebe54fb70c3834a2e5e5030ffd0525b5e0
ke-kishimoto <nebinosuk@gmail.com>
2022-08-06 19:00:51 +0900

    ヘッダーでメニューが表示されるように

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,24 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react'
 
 const Header = () => {
 	const navigate = useNavigate();
 
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
 	return (
+		<>
 		<Grid container spacing={2} margin={1}>
 			<Grid item xs={1}>
 				<IconButton
@@ -17,12 +30,13 @@ const Header = () => {
 					color="inherit"
 					aria-label="menu"
 					sx={{ mr: 2 }}
+					onClick={handleClick}
 				>
 					<MenuIcon />
 				</IconButton>
 			</Grid>
 			<Grid item xs={8} textAlign={'center'}>
-				<Link to="/top">タイトル</Link>
+				<Link to="/">タイトル</Link>
 			</Grid>
 			<Grid item xs={1}>
 				<Button 
@@ -41,6 +55,22 @@ const Header = () => {
 				</Button>
 			</Grid>
 		</Grid>
+
+		<Menu
+			anchorEl={anchorEl}
+			id="account-menu"
+			open={open}
+			onClose={handleClose}
+			onClick={handleClose}
+		>
+			<MenuItem>マイページ</MenuItem>
+			<MenuItem>参加イベント一覧</MenuItem>
+			<MenuItem>一括申し込み</MenuItem>
+			<MenuItem>問い合わせ</MenuItem>
+			<MenuItem>障害報告</MenuItem>
+			<MenuItem>ログアウト</MenuItem>
+		</Menu>
+		</>
 	)
 }
 
