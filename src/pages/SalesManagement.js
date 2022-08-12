a9d2e9707aab7393c54158f82109a5cbfbe471fb
ke-kishimoto <nebinosuk@gmail.com>
2022-08-12 21:46:28 +0900

    first deploy

---
+++ src/pages/SalesManagement.js
@@ -0,0 +1,13 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+const SalesManagement = () => {
+
+  return (
+    <Container>
+      <Typography>売上管理</Typography>
+    </Container>
+  )
+}
+
+export default SalesManagement;
