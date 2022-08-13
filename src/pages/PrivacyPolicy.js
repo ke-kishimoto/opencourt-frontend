304c177cf0978d22f3fb8582b1d9a70b74b6ad01
ke-kishimoto <nebinosuk@gmail.com>
2022-08-13 21:37:50 +0900

    プライバシーポリシー表示追加

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,7 +1,22 @@
+import {useState, useEffect} from 'react';
+import Typography from '@mui/material/Typography';
+import { useAxios } from '../utils/axiosUtil';
+
 const PrivacyPolicy = () => {
+
+  const [privacyPolicy, setPrivacyPolicy] = useState('');
+  const axios = useAxios();
+  useEffect(() => {
+    const fetchDate = async () => {
+      const result = await axios.get('/privacyPolicy');
+      setPrivacyPolicy(result.data.content);
+    }
+    fetchDate();
+  }, [])
+
 	return (
 		<>
-			プライバシーポリシー
+			<Typography><pre>{privacyPolicy}</pre></Typography>
 		</>
 	)
 }
