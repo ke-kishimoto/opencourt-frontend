b4b5d9785335f2cf21f39c47c05de24b05616f13
ke-kishimoto <nebinosuk@gmail.com>
2022-08-13 21:30:45 +0900

    プライバシー管理追加

--- src/pages/PrivacyPolicyMnagement.js
+++ src/pages/PrivacyPolicyMnagement.js
@@ -1,10 +1,27 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import RegisterBtn from '../elements/RegistBtn';
 import Grid from '@mui/material/Grid';
+import { useState, useEffect } from 'react';
+import { useAxios } from '../utils/axiosUtil';
 
 const PrivacyPolicyManagement = () => {
+
+  const axios = useAxios();
+  const [content, setContent] = useState('');
+  useEffect(() => {
+    const fetchDate = async () => {
+      const result = await axios.get('/privacyPolicy');
+      setContent(result.data.content);
+    }
+    fetchDate();
+  }, [])
+
+  const validation = () => {
+    return true;
+  }
+
 	return (
 		<Container maxWidth={'lg'}>
 			<Grid container spacing={5} margin={1}>
@@ -17,16 +34,19 @@ const PrivacyPolicyManagement = () => {
 						rows={20}
 						fullWidth
 						label="プライバシーポリシー"
-						variant="outlined"
+            variant="outlined"
+            value={content}
+            onChange={(e) => {setContent(e.target.value)}}
 					/>
 				</Grid>
 				<Grid item xs={10}/>
 				<Grid item xs={2}>
-					<Button
-						variant="contained"
-					>
-						更新
-					</Button>
+					<RegisterBtn
+            mode={'new'}
+            endpoint={'/privacyPolicy'}
+            validation={validation}
+            data={{content: content}}
+					/>
 				</Grid>
 			</Grid>
 		</Container>
