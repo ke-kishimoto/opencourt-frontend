ba74c387115b1efa6754a8152301cfabb45eebc7
ke-kishimoto <nebinosuk@gmail.com>
2022-08-15 15:31:42 +0900

    イベントの一覧取得を追加

--- src/pages/EventManagement.js
+++ src/pages/EventManagement.js
@@ -2,10 +2,25 @@ import Container from '@mui/material/Container';
 import SearchForm from '../forms/SearchForm';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { useAxios } from "../utils/axiosUtil";
+import { useState, useEffect } from 'react';
+import EventItem from '../components/EventItem';
+import Grid from '@mui/material/Grid';
 
 const EventManagement = () => {
 
-	const navigate = useNavigate();
+  const axios = useAxios();
+  const navigate = useNavigate();
+  
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/searchEvent');
+      setEvents(result.data);
+    }
+    fetchData();
+  }, [])
 
   return (
     <Container maxWidth={'lg'}>
@@ -16,6 +31,19 @@ const EventManagement = () => {
       >
       新規登録
       </Button>
+      <Grid container spacing={5} marginTop={5}>
+      {events.map(e => {
+        return (
+          <Grid item xs={12}
+            key={e.id}
+          >
+          <EventItem 
+            event={e}
+          />
+          </Grid>
+        )
+        })}
+      </Grid>
     </Container>
   )
 
