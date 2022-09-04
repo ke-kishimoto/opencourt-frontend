c1c4f87b36d6f184cdd642ec9bcc547e386fe5db
ke-kishimoto <nebinosuk@gmail.com>
2022-09-04 17:27:58 +0900

    LINE通知取得者数追加

--- src/pages/SegmentDelivery.js
+++ src/pages/SegmentDelivery.js
@@ -3,51 +3,45 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useState, useEffect } from 'react';
-
+import { useAxios } from '../utils/axiosUtil';
 
 const SegmentDelivery = () => {
 
-	const [events, setEents] = useState([]);
+  const axios = useAxios();
+  const [events, setEvents] = useState([]);
+  const [targetUserCount, setTargetUserCount] = useState(0);
 
 	useEffect(() => {
-		setEents([
-			{id:1, event_name: 'バスケ'},
-			{id:2, event_name: 'サッカー'},
-		])
-	}, [])
+		const fetchData = async () => {
+      const year = new Date().getFullYear();
+      const month = new Date().getMonth();
+      const result = await axios.get('/getEventByMonth/' + year + '/' + String((month + 1)).padStart(2, '0'));
+      setEvents(result.data)
+    }
+    fetchData();
+  }, [])
+  
+  const onChange = async (event) => {
+    const result = await axios.get('/getLinePushTargetUserCount/' + event.target.value)
+    setTargetUserCount(result.data);
+  }
 
 	return (
 		<Container maxWidth={'lg'}>
 			<Grid container spacing={5} margin={1}>
-				<Grid item xs={12}>
-					<Typography>セグメント配信</Typography>
-				</Grid>
-				<Grid item xs={12}>
-					<FormControlLabel control={<Checkbox />} label="カテゴリ1" />
-					<FormControlLabel control={<Checkbox />} label="カテゴリ2" />
-					<FormControlLabel control={<Checkbox />} label="カテゴリ3" />
-					<FormControlLabel control={<Checkbox />} label="カテゴリ4" />
-					<FormControlLabel control={<Checkbox />} label="カテゴリ5" />
-				</Grid>
-				<Grid item xs={12}>
-					<FormControlLabel control={<Checkbox />} label="男性" />
-					<FormControlLabel control={<Checkbox />} label="女性" />
-				</Grid>
 				<Grid item xs={12}>
 				<FormControl sx={{ width: '50%' }}>
 						<InputLabel>対象イベント</InputLabel>
 						<Select
 							required
 							name="event"
-							label="対象イベント"
+              label="対象イベント"
+              onChange={onChange}
 						>
 							{events.map(e => {
 								return (
@@ -55,7 +49,7 @@ const SegmentDelivery = () => {
 										value={e.id}
 										key={e.id}
 									>
-										{e.event_name}
+										{e.short_title + ' : ' + e.event_date}
 									</MenuItem>
 								);
 							})}
@@ -63,7 +57,7 @@ const SegmentDelivery = () => {
 					</FormControl>
 				</Grid>
 				<Grid item xs={12}>
-					<Typography>配信対象人数：xxx</Typography>
+            <Typography>配信対象人数：{targetUserCount}</Typography>
 				</Grid>
 				<Grid item xs={12}>
 					<TextField
