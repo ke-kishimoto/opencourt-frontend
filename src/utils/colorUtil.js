d90b3d43c7a7c82ae1bf3ae7ef969d97790cda56
ke-kishimoto <nebinosuk@gmail.com>
2022-09-04 16:08:37 +0900

    colorコードの取得変更

--- src/utils/colorUtil.js
+++ src/utils/colorUtil.js
@@ -1,9 +1,10 @@
-const COLOR_STATUS_PARTICIPATED = 1;
-const COLOR_STATUS_PARTICIPATED_WAITING = 2;
-const COLOR_STATUS_AFFORD = 3;
-const COLOR_STATUS_FEW = 4;
-const COLOR_STATUS_WAITING = 5;
-const COLOR_STATUS_END = 9;
+const COLOR_STATUS_PARTICIPATED = 'participation';
+const COLOR_STATUS_PARTICIPATED_WAITING = 'cancel_waiting';
+const COLOR_STATUS_AFFORD = 'afford';
+const COLOR_STATUS_FEW = 'few';
+const COLOR_STATUS_WAITING = 'waiting';
+const COLOR_STATUS_END = 'end';
+const COLOR_STATUS_BLACKLIST = 'blacklist';
 
 const COLOR_CODE_PARTICIPATED = '#007bff';
 const COLOR_CODE_PARTICIPATED_WAITING = '#DE5021';
@@ -11,6 +12,7 @@ const COLOR_CODE_AFFORD = '#0C9';
 const COLOR_CODE_FEW = '#FFCC33';
 const COLOR_CODE_WAITING = '#FF367F';
 const COLOR_CODE_END = '#BBB';
+const COLOR_CODE_BLACKLIST = '#000';
 
 const getStatusColorCode = (status) => {
   switch (status) {
